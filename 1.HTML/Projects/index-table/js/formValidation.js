e6c0b396eba7acc8c7c7f05f4a07c5b1d59894f3
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2020-08-14 18:03:09 +0100

    finished indexed db module

--- 1.HTML/Projects/index-table/js/formValidation.js
+++ 1.HTML/Projects/index-table/js/formValidation.js
@@ -1,54 +1,156 @@
 var formModule = (function () {
     'use strict';
 
-    var submitDetails = [
-        { "buttonId": "idSubmit", "inputId": "deleteByEmailKey", "columnLookup": "id" },
-        { "buttonId": "customerNameSubmit", "inputId": "deleteByCustomerName", "columnLookup": "customerName" },
-        { "buttonId": "companyNameSubmit", "inputId": "deleteByCompanyName", "columnLookup": "companyName" },
-        { "buttonId": "emailSubmit", "inputId": "deleteByEmailKey", "columnLookup": "email" },
-        { "buttonId": "phoneSubmit", "inputId": "deleteByPhone", "columnLookup": "phone" },
-        { "buttonId": "itemSubmit", "inputId": "deleteByItem", "columnLookup": "item" },
-        { "buttonId": "purchaseNumberSubmit", "inputId": "deleteByPurchaseNumber", "columnLookup": "purchaseNumber" }
-    ]
-    
+    var addInputIds = [
+        "inputAdd-customerName",
+        "inputAdd-companyName",
+        "inputAdd-email",
+        "inputAdd-phone",
+        "inputAdd-item",
 
-    function _submitButtonEvents(elementID, columnLookup) {
+    ]
+
+    function _checkInputValidAndPerformAction(elementID, submitDatafunc) {
+
         var input = document.getElementById(elementID);
-        console.log(input)
+
         if (!input.checkValidity()) {
             var form = input.parentElement.parentElement;
-            form.className = "was-validated";
+            form.className += " was-validated";
         }
         else {
-            createD.deletecustomerByColumn(columnLookup, input.value);
+            console.log("valid")
+            submitDatafunc(input.value)
         }
+    };
 
+    function createValidityArray(dataObj) {
+        var dataObjLength = Object.keys(dataObj).length;
+        return new Array(dataObjLength).fill(false);
     }
 
 
-    function CreateEvents() {
+    // get all search buttons
+    // provide more documentation
+    function CreateEventsWithAction(submitDetails, parseData) {
 
         for (var item in submitDetails) {
-  
+
             var BtnSubmit = document.getElementById(submitDetails[item].buttonId);
-            
+
             var submitItem = submitDetails[item];
 
-            (function(submitItem){
-                BtnSubmit.addEventListener("click", function () {
-                    console.log(submitItem)
-       
-                    _submitButtonEvents(submitItem.inputId, submitItem.columnLookup)
-                });
-              })(submitItem)
+            if (BtnSubmit) {
+
+                (function (submitItem) {
+                    BtnSubmit.addEventListener("click", function (event) {
+
+                        event.preventDefault();
+
+                        _checkInputValidAndPerformAction(submitItem.inputId, function (inputValue) {
+                            parseData(submitItem, inputValue)
+                        });
+                    });
+                })(submitItem)
+            }
         }
+    }
+
+    function addForm(submitElementId, dataObj) {
+
+        var submitBtn = document.getElementById(submitElementId);
+        if (submitBtn) {
+            submitBtn.addEventListener("click", function (event) {
+
+                event.preventDefault();
+
+                var formValidityArr = createValidityArray(dataObj);
+
+                // loop inputs 
+                for (var i = 0; i < addInputIds.length; i++) {
+
+                    _checkInputValidAndPerformAction(addInputIds[i], function (inputValue) {
+
+                        var columnName = addInputIds[i].split("-")[1]
+
+                        // set customer obj field value
+                        dataObj[columnName] = inputValue
+
+                        // set array valid 
+                        formValidityArr[i] = true;
+
+                    })
+                }
+
+                // check data is valid
+                var isDataValid = formValidityArr.every(function (value) {
+                    return value === true;
+                })
+
+                console.log(isDataValid)
+
+                if (isDataValid) {
+
+                    createD.add(dataObj);
+                }
+
+                else {
+
+                    // empty values
+                    Object.keys(dataObj).forEach(function (key, value) {
+                        return dataObj[key] = "";
+                    })
+
+                }
+            })
+
+        }
+
     }
 
     return {
-        CreateEvents: CreateEvents,
+        CreateEventsWithAction: CreateEventsWithAction,
+        addForm: addForm
 
     };
 })();
 
 
-formModule.CreateEvents();
+// fields to validate
+var customerObj = {
+    customerName: "",
+    companyName: "",
+    email: "",
+    phone: "",
+    item: "",
+}
+
+
+
+var submitDetails = [
+    { "buttonId": "idSubmit", "inputId": "deleteByID", "columnLookup": "id" },
+    { "buttonId": "customerNameSubmit", "inputId": "deleteByCustomerName", "columnLookup": "customerName" },
+    { "buttonId": "companyNameSubmit", "inputId": "deleteByCompanyName", "columnLookup": "companyName" },
+    { "buttonId": "emailSubmit", "inputId": "deleteByEmailKey", "columnLookup": "email" },
+    { "buttonId": "phoneSubmit", "inputId": "deleteByPhone", "columnLookup": "phone" },
+    { "buttonId": "itemSubmit", "inputId": "deleteByItem", "columnLookup": "item" },
+    { "buttonId": "purchaseNumberSubmit", "inputId": "deleteByPurchaseNumber", "columnLookup": "purchaseNumber" }
+];
+
+formModule.CreateEventsWithAction(submitDetails, function(submitItem, inputValue){
+    createD.deletecustomerByColumn(submitItem.columnLookup, inputValue);
+});
+
+
+formModule.addForm("addCustomer", customerObj);
+
+
+var searchSubmitDetails = [
+    { "buttonId": "idSearchSubmit", "inputId": "searchByID" }
+];
+
+
+formModule.CreateEventsWithAction(searchSubmitDetails, function(submitItem, inputValue){
+
+    createD.searchKey(inputValue);
+});
