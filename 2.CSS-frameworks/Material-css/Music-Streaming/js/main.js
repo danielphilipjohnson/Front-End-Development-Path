6530e01d5020fd42d56581d31cb97972cee38a95
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2020-11-15 20:06:34 +0000

    Attempted to fix cross site scripting issue with material css

--- 2.CSS-frameworks/Material-css/Music-Streaming/js/main.js
+++ 2.CSS-frameworks/Material-css/Music-Streaming/js/main.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
-    $('.carousel').carousel();
-    $('.modal').modal({
-        dismissible: true, // Modal can be dismissed by clicking outside of the modal
-        opacity: 1, // Opacity of modal background
-        inDuration: 300, // Transition in duration
-        outDuration: 200, // Transition out duration
-        startingTop: '0%', // Starting top style attribute
-        endingTop: '0%', // Ending top style attribute
-        ready: function (modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
-            console.log(modal, trigger);
-        },
-        complete: function () {} // Callback for Modal close
-    });
-})
+  $(".carousel").carousel();
+  $(".modal").modal({
+    dismissible: true, // Modal can be dismissed by clicking outside of the modal
+    opacity: 1, // Opacity of modal background
+    inDuration: 300, // Transition in duration
+    outDuration: 200, // Transition out duration
+    startingTop: "0%", // Starting top style attribute
+    endingTop: "0%", // Ending top style attribute
+    ready: function (modal, trigger) {
+      // Callback for Modal open. Modal and trigger parameters available.
+      console.log(modal, trigger);
+    },
+    complete: function () {}, // Callback for Modal close
+  });
+});
