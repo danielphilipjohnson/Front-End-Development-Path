d7531ce5e7b6b3d51698a662f9c9f291e7ca4dab
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-01-02 12:23:25 +0000

    Restructured folder

---
+++ 3.React-Projects/Future Projects/movie-db/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import SearchMovie from "./components/searchMovie";
+
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">React Movie Search</h1>
+      <SearchMovie />
+    </div>
+  );
+}
+
+export default App;
