73ada336a934ec79bbd40f10e3660da47ffa0fa5
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-01-07 16:41:10 +0000

    Updated config files

--- 3.React-Projects/Gatsby/basic-portfolio/gatsby-config.js
+++ 3.React-Projects/Gatsby/basic-portfolio/gatsby-config.js
@@ -5,12 +5,43 @@ module.exports = {
     author: `@gatsbyjs`,
     profileImage:
       "https://pbs.twimg.com/profile_images/1339602638111830020/k0dGL2Ye_400x400.png",
-    githubLink: "https://github.com/danielphilipjohnson",
-    twitterLink: "https://twitter.com/danielp_johnson",
-    linkedinLink: "https://www.linkedin.com/in/daniel-philip-johnson/",
-    codepenLink: "https://codepen.io/danielphilipjohnson/",
-    stackoverflowLink:
-      "https://stackoverflow.com/users/13921677/daniel-philip-johnson",
+    socials: [
+      {
+        link: "https://github.com/danielphilipjohnson",
+        FontAwesomeIcon: {
+          type: "fab",
+          logo: "github",
+        },
+      },
+      {
+        link: "https://twitter.com/danielp_johnson",
+        FontAwesomeIcon: {
+          type: "fab",
+          logo: "twitter",
+        },
+      },
+      {
+        link: "https://www.linkedin.com/in/daniel-philip-johnson/",
+        FontAwesomeIcon: {
+          type: "fab",
+          logo: "linkedin",
+        },
+      },
+      {
+        link: "https://codepen.io/danielphilipjohnson/",
+        FontAwesomeIcon: {
+          type: "fab",
+          logo: "codepen",
+        },
+      },
+      {
+        link: "https://stackoverflow.com/users/13921677/daniel-philip-johnson",
+        FontAwesomeIcon: {
+          type: "fab",
+          logo: "stack-overflow",
+        },
+      },
+    ],
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -23,6 +54,14 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/markdown-pages`,
+        name: `markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
--- 3.React-Projects/Gatsby/basic-portfolio/gatsby-node.js
+++ 3.React-Projects/Gatsby/basic-portfolio/gatsby-node.js
@@ -3,5 +3,45 @@
  *
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
-// You can delete this file if you're not using it
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `/` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allMarkdownRemark(filter: { frontmatter: { type: { eq: "blog" } } }) {
+        nodes {
+          frontmatter {
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  result.data.allMarkdownRemark.nodes.forEach(node => {
+    console.log(JSON.stringify(node.frontmatter, null, 4))
+    createPage({
+      path: node.frontmatter.slug,
+      component: path.resolve(`./src/template/blogTemplate.js`),
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        slug: node.frontmatter.slug,
+      },
+    })
+  })
+}
