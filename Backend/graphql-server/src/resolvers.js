ed3ab94595cd28f24d39bd58fafbc4b41cb7d09f
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-01-08 11:53:02 +0000

    Improved old server

--- Backend/graphql-server/src/resolvers.js
+++ Backend/graphql-server/src/resolvers.js
@@ -1,32 +1,98 @@
 import { users } from "./db";
 
+const Student = {
+  fullName: (root, args, context, info) => {
+    return root.firstName + ":" + root.lastName;
+  },
+};
+
 const resolvers = {
   Query: {
+    greeting: () => {
+      return "hello from  Dans world !!!";
+    },
     user: (parent, { id }, context, info) => {
+      console.log(users);
+      // need to check it exists
       return users.find((user) => user.id === id);
     },
+    userByUniversity: (parent, { univeristy }, context, info) => {
+      console.log(univeristy);
+      // need to check it exists
+      return users.find((user) => user.univeristy === univeristy);
+    },
+
     users: (parent, args, context, info) => {
       return users;
     },
   },
 
   Mutation: {
-    createUser: (parent, { id, name, email, age }, context, info) => {
-      const newUser = { id, name, email, age };
+    createUser: (
+      parent,
+      {
+        id,
+        first_name,
+        last_name,
+        email,
+        gender,
+        language,
+        race,
+        job_title,
+        skills,
+        university,
+      },
+      context,
+      info
+    ) => {
+      const newUser = {
+        first_name,
+        last_name,
+        email,
+        gender,
+        language,
+        race,
+        job_title,
+        skills,
+        university,
+      };
 
       users.push(newUser);
 
       return newUser;
     },
-    updateUser: (parent, { id, name, email, age }, context, info) => {
+
+    updateUser: (
+      parent,
+      {
+        first_name,
+        last_name,
+        email,
+        gender,
+        language,
+        race,
+        job_title,
+        skills,
+        university,
+      },
+      context,
+      info
+    ) => {
       let newUser = users.find((user) => user.id === id);
 
-      newUser.name = name;
+      newUser.first_name = first_name;
+      newUser.last_name = last_name;
       newUser.email = email;
-      newUser.age = age;
+      newUser.gender = gender;
+      newUser.language = language;
+      newUser.race = race;
+      newUser.job_title = job_title;
+      newUser.skills = skills;
+      newUser.university = university;
 
       return newUser;
     },
+
     deleteUser: (parent, { id }, context, info) => {
       const userIndex = users.findIndex((user) => user.id === id);
 
