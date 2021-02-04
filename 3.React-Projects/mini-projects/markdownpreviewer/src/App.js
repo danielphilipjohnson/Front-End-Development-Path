99d982897e12e860d7197c181010f08d874fa9fc
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-02-04 12:31:23 +0000

    indexed homepage

--- 3.React-Projects/mini-projects/markdownpreviewer/src/App.js
+++ 3.React-Projects/mini-projects/markdownpreviewer/src/App.js
@@ -1,28 +1,11 @@
 import React from "react";
 
-import Layout from "./components/layout/index";
-
-import BlogType from "./components/Blog/form/index";
-
-import MarkdownContainer from "./components/Markdown/MarkdownContainer";
-
-import BlogPost from "./components/Blog/post/index";
+import HomePage from "./routes/home/index";
 
 import "./App.css";
 
 const App = () => {
-  return (
-    <>
-      <Layout>
-        <BlogType />
-
-        <div className="posts d-flex flex-row flex-wrap">
-          <MarkdownContainer />
-          <BlogPost />
-        </div>
-      </Layout>
-    </>
-  );
+  return <HomePage />;
 };
 
 export default App;
---
+++ 3.React-Projects/mini-projects/markdownpreviewer/src/routes/home/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Layout from "../../components/layout/index";
+
+import BlogType from "../../components/Blog/form/index";
+
+import MarkdownContainer from "../../components/Markdown/MarkdownContainer";
+
+import BlogPost from "../../components/Blog/post/index";
+
+const index = () => {
+  return (
+    <>
+      <Layout>
+        <BlogType />
+
+        <div className="posts d-flex flex-row flex-wrap">
+          <MarkdownContainer />
+          <BlogPost />
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default index;
