5634a50fdaaac480eb2d2edfb0cf452b7c0fb9bb
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-02-04 10:14:41 +0000

    removed old markdown library and replaced withy react markdown

--- 3.React-Projects/mini-projects/markdownpreviewer/src/components/Markdown/DisplayContainer.js
+++ 3.React-Projects/mini-projects/markdownpreviewer/src/components/Markdown/DisplayContainer.js
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
-var marked = require("marked");
+import ReactMarkdown from "react-markdown";
 
 class DisplayContainer extends Component {
-  rawMarkup(value) {
-    var rawMarkup = marked(value, { sanitize: true });
-    return { __html: rawMarkup };
-  }
   render() {
     return (
       <div className="created-blog">
         <h1>{this.props.title}</h1>
-        <p dangerouslySetInnerHTML={this.rawMarkup(this.props.newText)} />
+        <ReactMarkdown>{this.props.newText}</ReactMarkdown>
       </div>
     );
   }
