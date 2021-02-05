06462dc9804d9581974d4af399141e63a422c299
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-02-05 10:53:57 +0000

    created footer with basic styling

---
+++ 3.React-Projects/mini-projects/calculator/src/components/footer/footer.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer class="main__footer mt-5" id="main-footer">
+      <div class="container">
+        <div class="main__footer__top">
+          <div class="disclaimer">
+            <h4>Content Disclaimer</h4>
+            <p>
+              {" "}
+              The content of this project came from
+              <a
+                id="tribute-link"
+                target="_blank"
+                href="https://en.wikipedia.org/wiki/HP_20b"
+              >
+                Wikipedia
+              </a>
+              , and
+              <a href="https://support.hp.com/gb-en/product/hp-20b-business-consultant-financial-calculator/3732534">
+                HP
+              </a>
+            </p>
+          </div>
+
+          <ul class="social-icons-footer">
+            <li class="nav-item twitter">
+              <a href="https://twitter.com/danielp_johnson">
+                <i class="fab fa-twitter"></i>
+              </a>
+            </li>
+
+            <li class="nav-item facebook">
+              <a href="httphttp://127.0.0.1:5500/Free-Code-Camp-Portfolio-2018-2020/1.Responsive-Web-Design-Certification/5.Personal-Portfolio-Webpage/index.htmls://www.facebook.com/DanielPhilipJohnson">
+                <i class="fab fa-facebook"></i>
+              </a>
+            </li>
+
+            <li class="nav-item github">
+              <a href="https://github.com/danielphilipjohnson/">
+                <i class="fab fa-github"></i>
+              </a>
+            </li>
+
+            <li class="nav-item linkedin">
+              <a href="https://www.linkedin.com/in/daniel-philip-johnson/">
+                <i class="fab fa-linkedin"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="main__footer__bottom">
+          <p>
+            Copyright &copy; 2020. All Rights Reserved | Designed and built with{" "}
+            <span class="heart">❤️</span>
+            by <a href="">Daniel Philip Johnson </a>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- 3.React-Projects/mini-projects/calculator/src/components/layout/layout.js
+++ 3.React-Projects/mini-projects/calculator/src/components/layout/layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "../navbar/index";
-
+import Footer from "../footer/index";
 const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
       <main className="container">{children}</main>
+      <Footer />
     </>
   );
 };
