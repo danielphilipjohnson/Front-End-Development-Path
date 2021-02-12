1855ea08b4e9310f275057fddd0b591c58b2f8a3
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-02-12 15:43:41 +0000

    cleaned padbank file

--- 3.React-Projects/useReducer/drumkit/src/components/drum-machine/pad-bank/PadBank.js
+++ 3.React-Projects/useReducer/drumkit/src/components/drum-machine/pad-bank/PadBank.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import DrumButton from "./DrumButton";
 import { useGlobalContext } from "../../../context/context";
 import "./PadBank.css";
@@ -7,7 +7,7 @@ const PadBank = (props) => {
   const { currentSoundBank } = useGlobalContext();
   let soundFiles = currentSoundBank.soundFiles;
 
-  const drumButtons = soundFiles.map((drumObj, i, soundFilesArr) => {
+  const drumButtons = soundFiles.map((_, i, soundFilesArr) => {
     return (
       <DrumButton
         id={i}
@@ -29,31 +29,4 @@ const PadBank = (props) => {
   );
 };
 
-// class PadBank extends Component {
-//   render() {
-//     let soundFiles = this.props.currentBankSet.soundFiles;
-
-//     const drumButtons = soundFiles.map((drumObj, i, soundFilesArr) => {
-//       return (
-//         <DrumButton
-//           updateDisplaySoundFileName={this.props.updateDisplaySoundFileName}
-//           id={i}
-//           audioVolume={this.props.audioVolume}
-//           name={soundFilesArr[i].id}
-//           soundId={soundFilesArr[i].id}
-//           key={soundFilesArr[i].keyTrigger}
-//           keyCode={soundFilesArr[i].keyCode}
-//           keyTrigger={soundFilesArr[i].keyTrigger}
-//           source={soundFilesArr[i].url}
-//         />
-//       );
-//     });
-
-//     return (
-//       <div className="padBank">
-//         <div className="drum-buttons">{drumButtons}</div>
-//       </div>
-//     );
-//   }
-// }
 export default PadBank;
