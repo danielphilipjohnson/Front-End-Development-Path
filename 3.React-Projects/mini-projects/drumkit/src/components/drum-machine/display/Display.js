2a94da5949477290b6164080d69c6ff0a73d496d
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-02-12 11:05:33 +0000

    cleaned component filenames

--- 3.React-Projects/mini-projects/drumkit/src/components/drum-machine/display/Display.js
+++ 3.React-Projects/mini-projects/drumkit/src/components/drum-machine/display/Display.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
-import Controls from "./Controls/Controls";
-import VolumeControl from './Controls/VolumeControl';
+import Controls from "../controls/Controls";
+import VolumeControl from "../controls/VolumeControl";
 import "./Display.css";
-
 
 class Display extends Component {
   render() {
@@ -10,13 +9,16 @@ class Display extends Component {
       <header id="display" className="top">
         <div className="drum-display">
           <h2 className="drum-display__title">{this.props.text}</h2>
-          <img src="https://raw.githubusercontent.com/danielphilipjohnson/Free-Code-Camp-Portfolio-2018-2019/master/2.Front-End-Libraries-Certification/3.%20Build%20a%20Drum%20Machine/src/img/waves.webp" alt="Logo" />
+          <img
+            src="https://raw.githubusercontent.com/danielphilipjohnson/Free-Code-Camp-Portfolio-2018-2019/master/2.Front-End-Libraries-Certification/3.%20Build%20a%20Drum%20Machine/src/img/waves.webp"
+            alt="Logo"
+          />
 
           <div className="sample-bank">
             <p className="sample-bank__label">Tune</p>
             <input type="range" min="1" max="100" value="50" />
           </div>
-          
+
           <div className="sample-bank">
             <p className="sample-bank__label">Gain</p>
             <input type="range" min="1" max="100" value="50" />
