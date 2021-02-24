3626671e1cd48063c92f488259c997abe0ab89dc
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-02-24 16:23:41 +0000

    templated middle footer

---
+++ 3.React-Projects/useReducer/amazon-clone/src/components/footer/footer.js
@@ -0,0 +1,142 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="footer-top"></div>
+      <div className="footer-mid">
+        <ul>
+          <li>
+            <h4>Get to Know Us</h4>
+          </li>
+          <li>
+            <a href="#">Careers</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#">UK Modern Slavery Statement</a>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <h4>Make Money with Us</h4>
+          </li>
+          <li>
+            <a href="#">Sell on Amazon</a>
+          </li>
+          <li>
+            <a href="#">Sell Under Private Brands</a>
+          </li>
+          <li>
+            <a href="#">Sell on Amazon Business</a>
+          </li>
+          <li>
+            <a href="#">Sell on Amazon Handmade</a>
+          </li>
+          <li>
+            <a href="#">Sell on Amazon Launchpad</a>
+          </li>
+          <li>
+            <a href="#">Associates Programme</a>
+          </li>
+          <li>
+            <a href="#">Fulfilment by Amazon</a>
+          </li>
+          <li>
+            <a href="#">Seller Fulfilled Prime</a>
+          </li>
+          <li>
+            <a href="#">Advertise Your Products</a>
+          </li>
+          <li>
+            <a href="#">Independently Publish with Us</a>
+          </li>
+          <li>
+            <a href="#">Amazon Pay</a>
+          </li>
+          <li>
+            <a href="#">Host an Amazon Hub</a>
+          </li>
+          <li>
+            <a href="#">›See all</a>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <h4>Amazon Payment Methods</h4>
+          </li>
+          <li>
+            <a href="#">Amazon Platinum Mastercard</a>
+          </li>
+          <li>
+            <a href="#">Amazon Classic Mastercard</a>
+          </li>
+          <li>
+            <a href="#">Amazon Money Store</a>
+          </li>
+          <li>
+            <a href="#">Gift Cards</a>
+          </li>
+          <li>
+            <a href="#">Amazon Currency Converter</a>
+          </li>
+          <li>
+            <a href="#">Payment Methods Help</a>
+          </li>
+          <li>
+            <a href="#">Shop with Points</a>
+          </li>
+          <li>
+            <a href="#">Top Up Your Account</a>
+          </li>
+          <li>
+            <a href="#">Top Up Your Account in Store</a>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <h4>Let Us Help You</h4>
+          </li>
+          <li>
+            <a href="#">COVID-19 and Amazon</a>
+          </li>
+          <li>
+            <a href="#">Track Packages or View Orders</a>
+          </li>
+          <li>
+            <a href="#">Delivery Rates & Policies</a>
+          </li>
+          <li>
+            <a href="#">Amazon Prime</a>
+          </li>
+          <li>
+            <a href="#">Returns & Replacements</a>
+          </li>
+          <li>
+            <a href="#">Recycling</a>
+          </li>
+          <li>
+            <a href="#">Manage Your Content and Devices</a>
+          </li>
+          <li>
+            <a href="#">Amazon Mobile App</a>
+          </li>
+          <li>
+            <a href="#">Amazon Assistant</a>
+          </li>
+          <li>
+            <a href="#">Customer Service</a>
+          </li>
+        </ul>
+      </div>
+      <div className="footer-bottom"></div>
+    </footer>
+  );
+}
+
+export default Footer;
