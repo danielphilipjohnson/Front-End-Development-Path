80009277f15934366da705bc9d11daadf73dedf5
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-02-25 16:16:48 +0000

    removed accessiblity warnings for href tag

--- 3.React-Projects/useReducer/amazon-clone/src/components/sitewide/footer/index.jsx
+++ 3.React-Projects/useReducer/amazon-clone/src/components/sitewide/footer/index.jsx
@@ -14,17 +14,17 @@ function Footer() {
               <h4 className="footer__mid-title">Get to Know Us</h4>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Careers
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 About Us
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 UK Modern Slavery Statement
               </a>
             </li>
@@ -35,67 +35,67 @@ function Footer() {
               <h4 className="footer__mid-title">Make Money with Us</h4>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Sell on Amazon
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Sell Under Private Brands
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Sell on Amazon Business
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Sell on Amazon Handmade
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Sell on Amazon Launchpad
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Associates Programme
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Fulfilment by Amazon
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Seller Fulfilled Prime
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Advertise Your Products
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Independently Publish with Us
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Amazon Pay
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Host an Amazon Hub
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 ›See all
               </a>
             </li>
@@ -106,47 +106,47 @@ function Footer() {
               <h4 className="footer__mid-title">Amazon Payment Methods</h4>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Amazon Platinum Mastercard
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Amazon Classic Mastercard
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Amazon Money Store
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Gift Cards
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Amazon Currency Converter
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Payment Methods Help
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Shop with Points
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Top Up Your Account
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Top Up Your Account in Store
               </a>
             </li>
@@ -157,52 +157,52 @@ function Footer() {
               <h4 className="footer__mid-title">Let Us Help You</h4>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 COVID-19 and Amazon
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Track Packages or View Orders
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Delivery Rates & Policies
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Amazon Prime
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Returns & Replacements
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Recycling
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Manage Your Content and Devices
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Amazon Mobile App
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Amazon Assistant
               </a>
             </li>
             <li>
-              <a href="#" className="footer__mid-link">
+              <a href="/" className="footer__mid-link">
                 Customer Service
               </a>
             </li>
@@ -212,20 +212,19 @@ function Footer() {
       <div className="footer__bottom">
         <ul>
           <li>
-            <a href="#">Conditions of Use & Sale</a>
+            <a href="/">Conditions of Use & Sale</a>
           </li>
           <li>
-            <a href="#">Privacy Notice</a>
+            <a href="/">Privacy Notice</a>
           </li>
           <li>
-            <a href="#">Cookies Notice</a>
+            <a href="/">Cookies Notice</a>
           </li>
           <li>
-            <a href="#">Interest-Based Ads Notice</a>
+            <a href="/">Interest-Based Ads Notice</a>
           </li>
           <li className="footer__bottom-last">
-            {" "}
-            © 1996-2021, Amazon.com, Inc. or its affiliates
+            © 2021, Amazon.fake. This is a personal project
           </li>
         </ul>
       </div>
