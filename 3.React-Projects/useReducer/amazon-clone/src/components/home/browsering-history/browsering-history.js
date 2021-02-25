3b59f609e93b1a36ed5040ed512105b5a56989dd
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-02-25 13:11:20 +0000

    improved styling of browsering history

--- 3.React-Projects/useReducer/amazon-clone/src/components/home/browsering-history/browsering-history.js
+++ 3.React-Projects/useReducer/amazon-clone/src/components/home/browsering-history/browsering-history.js
@@ -15,77 +15,97 @@ function BrowseringHistory() {
     <div className="recommended__items__container">
       <div className="recommended__items__row">
         <h2 className="browsering__items-title">
-          Recommended items other customers often buy again
+          Inspired by your browsing history
         </h2>
 
         <div className="browsering__items-cards">
           <div className="browsering__items-product">
             <img src={item1} alt="product description" />
-            <div className="description">
+            <div className="browsering__items-description-text">
               <p>
                 AMD Ryzen 7 3700X Processor (8C/16T, 36 MB Cache, 4.4 GHz Max
                 Boost)
               </p>
-              <p>★ ★ ★ ★ 86,555</p>
-              <p>£288.99 </p>
+              <p>
+                <span className="rating">★ ★ ★ ★ </span> <span>86,555</span>
+              </p>
+
+              <p className="price">£288.99 </p>
             </div>
           </div>
 
           <div className="browsering__items-product">
             <img src={item2} alt="product description" />
-            <div className="description">
+            <div className="browsering__items-description-text">
               <p>
                 AMD Ryzen 7 3700X Processor (8C/16T, 36 MB Cache, 4.4 GHz Max
                 Boost)
               </p>
-              <p>★ ★ ★ ★ 86,555</p>
-              <p>£288.99 </p>
+              <p>
+                <span className="rating">★ ★ ★ ★ </span> <span>86,555</span>
+              </p>
+
+              <p className="price">£288.99 </p>
             </div>
           </div>
 
           <div className="browsering__items-product">
             <img src={item3} alt="product description" />
-            <div className="description">
+            <div className="browsering__items-description-text">
               <p>
                 AMD Ryzen 7 3700X Processor (8C/16T, 36 MB Cache, 4.4 GHz Max
                 Boost)
               </p>
-              <p>★ ★ ★ ★ 86,555</p>
-              <p>£288.99 </p>
+              <p>
+                <span className="rating">★ ★ ★ ★ </span> <span>86,555</span>
+              </p>
+
+              <p className="price">£288.99 </p>
             </div>
           </div>
 
           <div className="browsering__items-product">
             <img src={item4} alt="product description" />
-            <p>
-              AMD Ryzen 7 3700X Processor (8C/16T, 36 MB Cache, 4.4 GHz Max
-              Boost)
-            </p>
-            <p>★ ★ ★ ★ 86,555</p>
-            <p>£288.99 </p>
-          </div>
-
-          <div className="browsering__items-product">
-            <img src={item5} alt="product description" />
-            <div className="description">
+            <div className="browsering__items-description-text">
               <p>
                 AMD Ryzen 7 3700X Processor (8C/16T, 36 MB Cache, 4.4 GHz Max
                 Boost)
               </p>
-              <p>★ ★ ★ ★ 86,555</p>
-              <p>£288.99 </p>
+              <p>
+                <span className="rating">★ ★ ★ ★ </span> <span>86,555</span>
+              </p>
+
+              <p className="price">£288.99 </p>
+            </div>
+          </div>
+
+          <div className="browsering__items-product">
+            <img src={item5} alt="product description" />
+            <div className="browsering__items-description-text">
+              <p>
+                AMD Ryzen 7 3700X Processor (8C/16T, 36 MB Cache, 4.4 GHz Max
+                Boost)
+              </p>
+              <p>
+                <span className="rating">★ ★ ★ ★ </span> <span>86,555</span>
+              </p>
+
+              <p className="price">£288.99 </p>
             </div>
           </div>
 
           <div className="browsering__items-product">
             <img src={item6} alt="product description" />
-            <div className="description">
+            <div className="browsering__items-description-text">
               <p>
                 AMD Ryzen 7 3700X Processor (8C/16T, 36 MB Cache, 4.4 GHz Max
                 Boost)
               </p>
-              <p>★ ★ ★ ★ 86,555</p>
-              <p>£288.99 </p>
+              <p>
+                <span className="rating">★ ★ ★ ★ </span> <span>86,555</span>
+              </p>
+
+              <p className="price">£288.99 </p>
             </div>
           </div>
         </div>
