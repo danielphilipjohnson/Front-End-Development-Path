72085c499f7cf7f95f3505c80a12823f9e3eb2ce
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-05-31 10:48:33 +0100

    enhancement: created a mock fetch to retrieve ads

--- Js-Projects/ads-carousel/dist/js/main.js
+++ Js-Projects/ads-carousel/dist/js/main.js
@@ -1,53 +1,67 @@
-// move data outside so others can provide it.
-// with a promise
+//
+const fetchAdsMock = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const statusObj = { status: 200 };
+
+    const { status } = statusObj;
+
+    if (status == 200) {
+      resolve([
+        {
+          id: 1,
+          name: "freeCodeCamp",
+          logo: "<i class='fa fa-free-code-camp' aria-hidden='true'></i>",
+          img: "./img/FreeCodeCampLogo.png",
+          text: "Learn to code — for free. Build projects. Earn certifications.",
+          url: "https://www.freecodecamp.org/",
+        },
+        {
+          id: 2,
+          name: "freeCodeCamp Youtube",
+          logo: "<i class='fa fa-free-code-camp' aria-hidden='true'></i>",
+          img: "./img/freeCodeCampYt.png",
+          text: "Technical courses",
+          url: "https://www.youtube.com/channel/UC8butISFwT-Wl7EV0hUK0BQ",
+        },
+        {
+          id: 3,
+          name: "freeCodeCamp News",
+          logo: "<i class='fa fa-free-code-camp' aria-hidden='true'></i>",
+          img: "./img/freeCodeCampNews.png",
+          text: "Keep up to date with the latest trends",
+          url: "https://www.freecodecamp.org/news/",
+        },
+        {
+          id: 4,
+          name: "freeCodeCamp.org",
+          logo: "<i class='fa fa-free-code-camp' aria-hidden='true'></i>",
+          img: "./img/freeCodeCampTwitter.png",
+          text: "Find out when we post new content by following us on twitter.",
+          url: "https://twitter.com/freeCodeCamp",
+        },
+      ]);
+    } else {
+      reject(` Request failed with ${status}. Could not fetch ads`);
+    }
+  }, 300);
+});
 
 class Carousel {
-  constructor() {
+  constructor(ads) {
     this.currentItem = 0;
+    this.ads = ads;
 
+    // Elements of the carosel to update
     this.adImage = document.getElementById("ad-image");
     this.adName = document.getElementById("ad-name");
     this.adText = document.getElementById("ad-text");
     this.adUrl = document.getElementById("ad-url");
 
+    // buttons to navigate ads
     this.prevBtn = document.getElementById("prev-ad");
     this.nextBtn = document.getElementById("next-ad");
 
-    this.ads = [
-      {
-        id: 1,
-        name: "freeCodeCamp",
-        logo: "<i class='fa fa-free-code-camp' aria-hidden='true'></i>",
-        img: "./img/FreeCodeCampLogo.png",
-        text: "Learn to code — for free. Build projects. Earn certifications.",
-        url: "https://www.freecodecamp.org/",
-      },
-      {
-        id: 2,
-        name: "freeCodeCamp Youtube",
-        logo: "<i class='fa fa-free-code-camp' aria-hidden='true'></i>",
-        img: "./img/freeCodeCampYt.png",
-        text: "Technical courses",
-        url: "https://www.youtube.com/channel/UC8butISFwT-Wl7EV0hUK0BQ",
-      },
-      {
-        id: 3,
-        name: "freeCodeCamp News",
-        logo: "<i class='fa fa-free-code-camp' aria-hidden='true'></i>",
-        img: "./img/freeCodeCampNews.png",
-        text: "Keep up to date with the latest trends",
-        url: "https://www.freecodecamp.org/news/",
-      },
-      {
-        id: 4,
-        name: "freeCodeCamp.org",
-        logo: "<i class='fa fa-free-code-camp' aria-hidden='true'></i>",
-        img: "./img/freeCodeCampTwitter.png",
-        text: "Find out when we post new content by following us on twitter.",
-        url: "https://twitter.com/freeCodeCamp",
-      },
-    ];
-
+    // events for navigating ads
     this.nextBtn.addEventListener("click", () => {
       this.currentItem++;
       if (this.currentItem > this.ads.length - 1) {
@@ -63,6 +77,9 @@ class Carousel {
       }
       this.showAd(this.currentItem);
     });
+
+    // display ad on intial render
+    this.showAd();
   }
 
   showAd() {
@@ -76,6 +93,11 @@ class Carousel {
 
 // load initial item
 window.addEventListener("DOMContentLoaded", function () {
-  const car = new Carousel();
-  car.showAd();
+  fetchAdsMock
+    .then((ads) => {
+      new Carousel(ads);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
