f7234e3fcb26fd7e84895275664fe635144163cc
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-06-01 14:37:02 +0100

    enhancement: video player is now functiona. Some refactoring needs to be done

--- Js-Projects/netflix-video/dist/js/main.js
+++ Js-Projects/netflix-video/dist/js/main.js
@@ -1,90 +1,222 @@
-class NetflixVideo {
-  constructor(videoElem, timerElem, rangeElem) {
-    this.video = videoElem || document.getElementById("video");
-
+class VideoTimer {
+  constructor(timerElem) {
     this.timer = timerElem || document.getElementById("timer");
+  }
+  // need to allow user some flexibility making custom
+  _updateTimer(videoDuration, videoCurrentTime) {
+    console.log(videoDuration);
+    this.timer.textContent = Math.round(videoDuration - videoCurrentTime);
+  }
+}
+
+class ProgressBar {
+  constructor(rangeElem, video) {
     this.range = rangeElem || document.querySelector(".range");
 
-    // Set default behaviour
-    this.video.currentTime = 0;
+    // split this into values
+    this.video = video;
     this.range.value = 0;
-    this.range.max = this.video.duration;
-
-    this.progressInterval = null;
-    this._updateTime();
-    this.createVideoSliderEvent();
-    this.clickVideoToPlay();
+    this.range.max = video.duration;
   }
 
-  _updateTime() {
-    this.timer.textContent = Math.round(
-      this.video.duration - this.video.currentTime
-    );
+  updateCurrentValue(newValue) {
+    this.range.value = newValue;
   }
 
-  updateVideoProgress(currentTime) {
-    let currentVideoPlace = currentTime;
+  createVideoSliderEvent(updateTime, callback) {
+    this.range.addEventListener("change", () => {
+      this.updateVideoProgress(this.range.value, callback);
 
+      this.video.currentTime = this.range.value;
+
+      if (typeof updateTime == "function")
+        updateTime(this.video.duration, this.video.currentTime);
+    });
+  }
+  // buffering should be prop on video
+  updateVideoProgress(currentVideoPlace, callback) {
     currentVideoPlace = (100 / this.video.duration) * currentVideoPlace;
 
     let buf = (100 - currentVideoPlace) / 4 + parseInt(currentVideoPlace);
 
-    // later make away for users to add their own style functionality
-    const updateProgressStyle = () => {
-      this.range.style =
-        "background:linear-gradient(to right, #cc181e 0%, #cc181e " +
-        currentVideoPlace +
-        "%, #777 " +
-        currentVideoPlace +
-        "%, #777 " +
-        buf +
-        "%, #444 " +
-        buf +
-        "%, #444 100%)";
-    };
-    // make a new function
-    updateProgressStyle();
+    if (typeof callback == "function")
+      callback(this.range, currentVideoPlace, buf);
+  }
+}
+
+class PlayableVideo {
+  constructor() {
+    this.videoElem = document.getElementById("video");
+    this.videoElem.currentTime = 0;
+
+    this.progressInterval = null;
+    this.range = null;
+    this.videoTimer = null;
   }
 
-  createVideoSliderEvent() {
-    this.range.addEventListener("mousemove", () => {
-      this.updateVideoProgress(this.range.value);
+  init(range, videoTimer) {
+    this.range = range;
+    console.log(this.range);
+    this.videoTimer = videoTimer;
+    this.clicked();
+  }
 
-      this.video.currentTime = this.range.value;
+  updateUI() {
+    this.range.updateCurrentValue(this.videoElem.currentTime);
 
-      this._updateTime();
+    // style dupication
+    this.range.updateVideoProgress(
+      this.range.range.value,
+      (range, currentVideoPlace, buf) => {
+        range.style =
+          "background:linear-gradient(to right, #cc181e 0%, #cc181e " +
+          currentVideoPlace +
+          "%, #777 " +
+          currentVideoPlace +
+          "%, #777 " +
+          buf +
+          "%, #444 " +
+          buf +
+          "%, #444 100%)";
+      }
+    );
+
+    this.videoTimer._updateTimer(
+      this.videoElem.duration,
+      this.videoElem.currentTime
+    );
+  }
+
+  videoEvents(onPlay, onPause) {
+    if (this.videoElem.paused) {
+      if (typeof onPlay == "function") onPlay();
+    } else {
+      if (typeof onPause == "function") onPause();
+    }
+  }
+
+  clicked() {
+    const playBtn = document.getElementById("play-btn");
+    const rewindBtn = document.getElementById("rewind-btn");
+    const forwardBtn = document.getElementById("forward-btn");
+    const VolumeBtn = document.getElementById("volume-btn");
+    const VolumeBar = document.getElementById("volume-range");
+
+    VolumeBar.addEventListener("change", (e) => {
+      this.videoElem.volume = e.target.value / 100;
     });
-  }
+    // move out
+    // VolumeBtn.addEventListener("click", () => {
+    //   if (this.videoElem.volume > 0) {
+    //     this.videoElem.volume = 0;
+    //     VolumeBtn.children[0].innerHTML = `<i class="fas fa-volume-mute"></i>`;
 
-  clickVideoToPlay() {
-    this.video.addEventListener("click", () => {
-      if (this.video.paused) {
-        this.video.play();
-        this.progressInterval = setInterval(this.f.bind(this), 1000);
-      } else {
-        this.video.pause();
+    //     // show mute
+    //   } else {
+    //     this.videoElem.volume = 1;
+    //     VolumeBtn.children[0].innerHTML = `<i class="fas fa-volume-up"></i>`;
+    //   }
+    // });
 
-        clearInterval(this.progressInterval);
+    const newVideoEvent = () => {
+      this.videoEvents(
+        () => {
+          playBtn.innerHTML = `<i class="fas fa-pause"></i>`;
+          this.videoElem.play();
+          this.progressInterval = setInterval(this.updateUI.bind(this), 1000);
+        },
+        () => {
+          playBtn.innerHTML = `<i class="fas fa-play"></i>`;
+          this.videoElem.pause();
+          clearInterval(this.progressInterval);
+        }
+      );
+    };
+
+    this.videoElem.addEventListener("click", newVideoEvent);
+
+    playBtn.addEventListener("click", newVideoEvent);
+
+    // repetitive
+    forwardBtn.addEventListener("click", () => {
+      if (!this.videoElem.paused) {
+        playBtn.innerHTML = `<i class="fas fa-play"></i>`;
+        this.videoElem.pause();
+        this.progressInterval = setInterval(this.updateUI.bind(this), 1000);
+      }
+      if (this.videoElem.currentTime < this.videoElem.duration) {
+        const step = this.videoElem.duration / 100;
+
+        this.videoElem.currentTime = (
+          this.videoElem.currentTime + step
+        ).toFixed(2);
+
+        this.updateUI();
+      }
+    });
+
+    rewindBtn.addEventListener("click", () => {
+      if (!this.videoElem.paused) {
+        playBtn.innerHTML = `<i class="fas fa-play"></i>`;
+        this.videoElem.pause();
+        this.progressInterval = setInterval(this.updateUI.bind(this), 1000);
+      }
+      if (this.videoElem.currentTime > 0) {
+        const step = this.videoElem.duration / 100;
+
+        this.videoElem.currentTime = (
+          this.videoElem.currentTime - step
+        ).toFixed(2);
+
+        this.updateUI();
       }
     });
   }
-  // play and pause btn
+}
+// buffering should be prop on video
 
-  // skip forward
+class NetflixVideo {
+  constructor(playableVideo, rangeElem, videoTimer) {
+    this.playableVideo = playableVideo;
+    this.video = playableVideo.videoElem;
 
-  // skip backwards
-  f() {
-    this.range.value = this.video.currentTime;
-    this.updateVideoProgress(this.range.value);
-    this._updateTime();
+    this.range = rangeElem || document.querySelector(".range");
+
+    this.videoTimer = videoTimer;
+
+    this.videoTimer._updateTimer(this.video.duration, this.video.currentTime);
+
+    this.progressBar = new ProgressBar(rangeElem, this.video);
+
+    this.playableVideo.init(this.progressBar, this.videoTimer);
+
+    this.progressBar.createVideoSliderEvent(
+      (videoDuration, videoCurrentTime) => {
+        this.videoTimer._updateTimer(videoDuration, videoCurrentTime);
+      },
+      (range, currentVideoPlace, buf) => {
+        range.style =
+          "background:linear-gradient(to right, #cc181e 0%, #cc181e " +
+          currentVideoPlace +
+          "%, #777 " +
+          currentVideoPlace +
+          "%, #777 " +
+          buf +
+          "%, #444 " +
+          buf +
+          "%, #444 100%)";
+      }
+    );
   }
 }
-// make state like redux for PAUSED
+
 // load initial item
 window.addEventListener("DOMContentLoaded", function () {
   new NetflixVideo(
-    document.getElementById("video"),
-    document.getElementById("timer"),
-    document.querySelector(".range")
+    new PlayableVideo(),
+    document.querySelector(".range"),
+    new VideoTimer()
   );
 });
+
+// once video made allow users to set controls.
