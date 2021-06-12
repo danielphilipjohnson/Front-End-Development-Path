747b3ae8cbdd6eddf99a0664af3eea8f13cb64c4
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-06-12 19:58:05 +0100

    made computed componenet

--- 6.vue-projects/tutorials/with-cli/app-cli/src/components/Greeting.vue
+++ 6.vue-projects/tutorials/with-cli/app-cli/src/components/Greeting.vue
@@ -2,6 +2,7 @@
   <button type="button" @click="onClickAge">Increase age</button>
   <h2 v-if="age >= 21">Greeting {{ age }}</h2>
   <h2 v-else>Greeting you must be 21 or over</h2>
+  <p>{{ ageDoubled }}</p>
 </template>
 
 <script>
@@ -10,6 +11,12 @@ export default {
   props: {
     age: Number,
   },
+  computed: {
+    ageDoubled() {
+      return this.age * 2;
+    },
+  },
+  emits: ["age-change"],
   methods: {
     onClickAge() {
       this.$emit("age-change", 3);
