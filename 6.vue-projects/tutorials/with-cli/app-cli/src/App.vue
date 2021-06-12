69faa3268c52c927ae267cae4d4e80dfac6508fd
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-06-12 19:47:58 +0100

    used components and props

--- 6.vue-projects/tutorials/with-cli/app-cli/src/App.vue
+++ 6.vue-projects/tutorials/with-cli/app-cli/src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Hello</h1>
+    <button type="button" @click="age++">Increase age</button>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Greeting :age="age" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import Greeting from "./components/Greeting.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    Greeting,
+  },
+  data() {
+    return {
+      age: 19,
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ 6.vue-projects/tutorials/with-cli/app-cli/src/components/Greeting.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2 v-if="age >= 21">Greeting {{ age }}</h2>
+  <h2 v-else>Greeting you must be 21 or over</h2>
+</template>
+
+<script>
+export default {
+  name: "Greeting",
+  props: {
+    age: Number,
+  },
+};
+</script>
