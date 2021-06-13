0b6478619b9a8f7cdff1c64aa3171b1fe0f690b6
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-06-13 20:37:16 +0100

    added subnav

--- 6.vue-projects/mini-projects/ad-caro/src/App.vue
+++ 6.vue-projects/mini-projects/ad-caro/src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <Nav />
+  <Subnav />
 </template>
 
 <script>
 import Nav from "./components/Nav.vue";
+import Subnav from "./components/Subnav.vue";
 export default {
   name: "App",
-  components: { Nav },
+  components: { Nav, Subnav },
 };
 </script>
 
