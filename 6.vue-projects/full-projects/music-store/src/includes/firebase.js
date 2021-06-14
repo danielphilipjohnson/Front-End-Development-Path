dfe011c0ea65c6e2b523c3bf38231506a047514c
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-06-14 19:11:15 +0100

    files can be stored and uploaded

--- 6.vue-projects/full-projects/music-store/src/includes/firebase.js
+++ 6.vue-projects/full-projects/music-store/src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+import "firebase/storage";
 
 var firebaseConfig = {
   apiKey: "AIzaSyAOGOugykRgedLT3A4_3Y-0LH4Rd4B46c4",
@@ -16,7 +17,8 @@ firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
 
 const usersCollection = db.collection("users");
 
-export { auth, db, usersCollection };
+export { auth, db, storage, usersCollection };
