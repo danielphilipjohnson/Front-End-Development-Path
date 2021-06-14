51882e14af18b917731471910c1aaa5c7f8ede16
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-06-14 09:48:04 +0100

    enhancement: added auth modal wioth basic state

--- 6.vue-projects/full-projects/music-store/src/App.vue
+++ 6.vue-projects/full-projects/music-store/src/App.vue
@@ -2,12 +2,15 @@
   <app-header />
 
   <router-view />
+  <auth-modal />
 </template>
 <script>
 import AppHeader from "./components/layout/Header.vue";
+import AuthModal from "./components/auth/AuthModal.vue";
 export default {
   components: {
     AppHeader,
+    AuthModal,
   },
 };
 </script>
