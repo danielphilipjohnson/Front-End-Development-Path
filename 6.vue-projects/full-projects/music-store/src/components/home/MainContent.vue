4d7aa3952f4fcfda7fcaf5049654b05c522c7eb7
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-06-14 09:40:52 +0100

    homepage built with tailwind CSS

---
+++ 6.vue-projects/full-projects/music-store/src/components/home/MainContent.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- Main Content -->
+  <section class="container mx-auto">
+    <div class="bg-white rounded border border-gray-200 relative flex flex-col">
+      <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
+        <span class="card-title">Songs</span>
+        <i class="fa fa-headphones-alt float-right text-green-400 text-2xl"></i>
+      </div>
+      <!-- Playlist seperate -->
+      <play-list />
+      <!-- .. end Playlist -->
+    </div>
+  </section>
+</template>
+
+<script>
+import PlayList from "../player/PlayList.vue";
+export default {
+  name: "MainContent",
+  components: {
+    PlayList,
+  },
+};
+</script>
--- 6.vue-projects/full-projects/music-store/src/views/Home.vue
+++ 6.vue-projects/full-projects/music-store/src/views/Home.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="home">
-    <p>dhfkjdshfjk</p>
-  </div>
+  <call-to-action />
+  <main-content />
+  <player-UI />
 </template>
 
 <script>
 // @ is an alias to /src
-
+import CallToAction from "../components/home/CallToAction.vue";
+import MainContent from "../components/home/MainContent.vue";
+import PlayerUI from "../components/player/PlayerUI.vue";
 export default {
   name: "Home",
-  components: {},
+  components: { CallToAction, MainContent, PlayerUI },
 };
 </script>
