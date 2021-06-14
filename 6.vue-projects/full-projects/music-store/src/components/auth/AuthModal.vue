c57262c0588c74eccca22c1ee58b287fba9002fc
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2021-06-14 09:59:40 +0100

    enhancement: login and register button relfect what mode the user is on

--- 6.vue-projects/full-projects/music-store/src/components/auth/AuthModal.vue
+++ 6.vue-projects/full-projects/music-store/src/components/auth/AuthModal.vue
@@ -62,6 +62,10 @@
                 class="block rounded py-3 px-4 transition"
                 href="#"
                 @click.prevent="tab = 'login'"
+                :class="{
+                  'hover:text-white text-white bg-blue-600': tab === 'login',
+                  'hover:text-blue-600': tab === 'register',
+                }"
               >
                 Login
               </a>
@@ -71,6 +75,10 @@
                 class="block rounded py-3 px-4 transition hover:text-blue-600"
                 href="#"
                 @click.prevent="tab = 'register'"
+                :class="{
+                  'hover:text-white text-white bg-blue-600': tab === 'register',
+                  'hover:text-blue-600': tab === 'login',
+                }"
                 >Register</a
               >
             </li>
